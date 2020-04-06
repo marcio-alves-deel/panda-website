0bf3908c3a9dd6c64a41423feaa4ee295184fb9f
marciowelben <marcio.alves.external@ynap.com>
2020-04-06 04:59:31 +0100

    build

---
+++ build/precache-manifest.45e59a904171754a27dd48ac2e024c25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252f21d1a6cd580695995907dde81c12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "822987828a9eeebb8ebd",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "822987828a9eeebb8ebd",
+    "url": "/static/js/2.590dca00.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.590dca00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d57405a9918c6978544",
+    "url": "/static/js/main.99f3912a.chunk.js"
+  },
+  {
+    "revision": "d70645987b4b3e1843e2",
+    "url": "/static/js/runtime-main.a5380112.js"
+  },
+  {
+    "revision": "1423942513778ca2bf6dbd32c7c7fb65",
+    "url": "/static/media/header-background.14239425.png"
+  }
+]);
