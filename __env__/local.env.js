29ae56eb2f4de5205044703688c62c2e24da7400
marciowelben <marcio.alves.external@ynap.com>
2020-04-18 21:25:34 +0100

    update base url

--- __env__/local.env.js
+++ __env__/local.env.js
@@ -2,7 +2,7 @@ module.exports = {
   meta: {},
   data: {
     API: {
-      BASE_URL: 'https://api.pandaragnarok.com',
+      BASE_URL: 'https://api.pandaragnarok.com/api',
       REQUEST_TIMEOUT: 15000,
       REQUEST_DELAY: 1000
     }
