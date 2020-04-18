5b03a930eea05d4aad42fe84d31ebe70e1b9ed99
marciowelben <marcio.alves.external@ynap.com>
2020-04-18 21:19:07 +0100

    update api base url

--- __env__/local.env.js
+++ __env__/local.env.js
@@ -2,7 +2,7 @@ module.exports = {
   meta: {},
   data: {
     API: {
-      BASE_URL: 'http://34.95.188.31/api',
+      BASE_URL: 'https://34.95.188.31/api',
       REQUEST_TIMEOUT: 15000,
       REQUEST_DELAY: 1000
     }
--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,7 @@
 module.exports = {
-  API: {
-    BASE_URL: 'http://34.95.188.31/api',
-    REQUEST_TIMEOUT: 15000,
-    REQUEST_DELAY: 1000
+  "API": {
+    "BASE_URL": "https://34.95.188.31/api",
+    "REQUEST_TIMEOUT": 15000,
+    "REQUEST_DELAY": 1000
   }
 }
